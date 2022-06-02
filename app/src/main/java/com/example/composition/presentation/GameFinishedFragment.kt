f5d780b70384f92172005f7734aa3e30514c8b3c
Dima <tamstroy@gmail.com>
2022-06-02 22:51:07 +0300

    add fragment manager

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.composition.databinding.FragmentGameFinishedBinding
+import com.example.composition.domain.entity.Level
 
 class GameFinishedFragment : Fragment() {
 
