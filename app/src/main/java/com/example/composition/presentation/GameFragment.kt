8068e236e79e815a871c1ff520aacc9e075c4b2d
Dima <tamstroy@gmail.com>
2022-06-03 23:09:01 +0300

    added  Parcelable

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -11,12 +11,12 @@ import com.example.composition.domain.entity.GameResult
 import com.example.composition.domain.entity.GameSettings
 import com.example.composition.domain.entity.Level
 
-class GameFragment:Fragment() {
+class GameFragment : Fragment() {
     private lateinit var level: Level
 
-    private var _binding:FragmentGameBinding? = null
+    private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
-    get()= _binding?: throw RuntimeException("FragmentGameBinding == null")
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +28,15 @@ class GameFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentGameBinding.inflate(inflater,container,false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvOption1.setOnClickListener(){
-            val settings = GameSettings(10,10,10,10)
-            val gameResult = GameResult(true,5,5,settings)
+        binding.tvOption1.setOnClickListener() {
+            val settings = GameSettings(10, 10, 10, 10)
+            val gameResult = GameResult(true, 5, 5, settings)
             launchGameFinishedFragment(gameResult)
         }
 
@@ -47,25 +47,27 @@ class GameFragment:Fragment() {
         _binding = null
     }
 
-    private fun parseArgs(){
-        level=requireArguments().getSerializable(KEY_LEVEL) as Level
+    private fun parseArgs() {
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
+        }
     }
 
-    private fun launchGameFinishedFragment (gameResult: GameResult){
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
         requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container,GameFinishedFragment.newInstance(gameResult))
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
             .addToBackStack(null)
             .commit()
     }
 
 
-    companion object{
-        private const val KEY_LEVEL  = "level"
+    companion object {
+        private const val KEY_LEVEL = "level"
         const val NAME = "GameFragment"
-        fun newInstance(level: Level):GameFragment{
+        fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
-                arguments=Bundle().apply {
-                    putSerializable(KEY_LEVEL,level)
+                arguments = Bundle().apply {
+                    putParcelable(KEY_LEVEL, level)
                 }
             }
         }
